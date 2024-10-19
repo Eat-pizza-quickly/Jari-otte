c9e3a23628f0a30eb443a654378a14cf9879a025
KangWookOh <kanguk456@gmail.com>
2024-10-20 03:43:22 +0900

    init:all code refactoring

--- src/main/java/com/eatpizzaquickly/jariotte/domain/seat/entity/seat.java
+++ src/main/java/com/eatpizzaquickly/jariotte/domain/seat/entity/seat.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class seat {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seat_id")
     private Long id;
     @Column(nullable = false)
     private String seatLocation;
