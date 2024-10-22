007697456ea5f59600ceded1481d7e885e079bd6
Woojin1123 <boom175839@gmail.com>
2024-10-22 21:08:46 +0900

    fix: merge conflict 해결

--- src/main/java/com/eatpizzaquickly/jariotte/domain/concert/entity/repository/ConcertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eatpizzaquickly.jariotte.domain.concert.entity.repository;
-
-import com.eatpizzaquickly.jariotte.domain.concert.entity.Concert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConcertRepository extends JpaRepository<Concert, Long> {
-}
