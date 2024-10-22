236d2c005715cf0a7f377e885ff68ae95f11e910
Woojin1123 <boom175839@gmail.com>
2024-10-22 17:48:01 +0900

    fix: import 수정

--- src/main/java/com/eatpizzaquickly/jariotte/domain/concert/service/ConcertService.java
+++ src/main/java/com/eatpizzaquickly/jariotte/domain/concert/service/ConcertService.java
@@ -13,10 +13,9 @@ import com.eatpizzaquickly.jariotte.domain.concert.repository.ConcertRepository;
 import com.eatpizzaquickly.jariotte.domain.concert.repository.VenueRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.awt.print.Pageable;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
